0dd44a5ab6e99c03e817a078ed4c8e7e6d8a895b
Philip James <pjj@philipjohnjames.com>
2013-11-23 22:57:31 -0800

    Initial commit.

---
+++ Whetstone-iOS/Whetstone-iOS/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Whetstone-iOS
+//
+//  Created by Philip James on 11/23/13.
+//  Copyright (c) 2013 Philip James. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Whetstone-iOS/Whetstone-iOSTests/Whetstone_iOSTests.m
@@ -0,0 +1,34 @@
+//
+//  Whetstone_iOSTests.m
+//  Whetstone-iOSTests
+//
+//  Created by Philip James on 11/23/13.
+//  Copyright (c) 2013 Philip James. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Whetstone_iOSTests : XCTestCase
+
+@end
+
+@implementation Whetstone_iOSTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
